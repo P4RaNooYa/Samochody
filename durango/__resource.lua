cd7c7300c0e6f9bfb7db12f01f7c773b2b9228cc
P4RaNooYa <70169875+P4RaNooYa@users.noreply.github.com>
2020-09-08 20:33:51 +0200

    Add files via upload

---
+++ durango/__resource.lua
@@ -0,0 +1,24 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5' --MADE BY ASSISTANT DEVELOPER MARK FOR NAMELESSRP, NO REDISTRIBUTION OR SELLING OF THIS ASSET. IT IS OWNED BY NAMLESSRP.-->
+ 
+files {
+    'vehicles.meta',
+    'carvariations.meta',
+    'carcols.meta',
+    'handling.meta',
+    'vehiclelayouts.meta',
+    'dlctext.meta'
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+data_file 'CARCOLS_FILE' 'carcols.meta'
+data_file 'DLCTEXT_FILE' 'dlctext.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'   -- Not Required
+
+
+client_script {
+    'vehicle_names.lua'    -- Not Required
+}
+
+--Discord.gg/namelessRP, website: Community.nlrp.xyz, play.nlrp.xyz, play2.nlrp.xyz, This asset is not to be sold or made any profit off it is only going to be used inside of NAMELESSRP.
